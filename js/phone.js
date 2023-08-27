bd2906b24231eb09a9dcb0dcc73e94218698fcde
najmul-islam <itzmenajmul@gmail.com>
2023-08-27 23:48:59 +0600

    add search bar and make dynamic search result

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json();
     const phones = data.data;
     displayPhones(phones)
@@ -8,27 +8,32 @@ const loadPhone = async () => {
 const displayPhones = phones => {
     
     const phoneContainer = document.getElementById('phone-container');
-
+    phoneContainer.textContent = '';
     phones.forEach(phone => {
         console.log(phone)
         // 1 create  a div
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card w-auto py-3 bg-gray-200 shadow-xl`;
         phoneCard.innerHTML = `
-        <figure>
-            <img src="${phone.image}">
-        </figure>
-        <div class="card-body">
-            <h2 class="card-title">${phone.phone_name}</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+            <figure>
+                <img src="${phone.image}">
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
             </div>
-        </div>
         `
         phoneContainer.appendChild(phoneCard)
-
     });
 }
 
-loadPhone()
+// handle search button
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhone(searchText)
+}
+// loadPhone()
